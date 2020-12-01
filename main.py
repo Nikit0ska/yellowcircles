c596e2c0fcacddc6a674aa026b81f2fc5efff2c7
Nikit0ska <nik.ryabov.2014@mail.ru>
2020-12-02 01:49:18 +0300

    random colors

--- main.py
+++ main.py
@@ -1,24 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'UI.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+import random
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QColor, QPainter
 import sys
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication
-import random
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(567, 464)
+        MainWindow.setMinimumSize(QtCore.QSize(567, 464))
+        MainWindow.setMaximumSize(QtCore.QSize(567, 464))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(120, 110, 291, 141))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
 
-class MyWidget(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("UI.ui", self)
+
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Кнопка"))
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent=parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.onClicked)
         self.flag = False
-        self.pushButton.clicked.connect(self.onClicked)
-
 
     def onClicked(self):
         self.flag = True
         self.update()
-
 
     def paintEvent(self, e):
         if self.flag:
@@ -27,20 +58,15 @@ class MyWidget(QMainWindow):
             self.drawEllipse(qp)
             qp.end()
 
-
     def drawEllipse(self, qp):
         x = random.randint(0, 567)
         y = random.randint(0, 464)
         radius = random.randint(0, 100)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
         qp.drawEllipse(x, y, radius, radius)
 
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    MW = MyWidget()
-    MW.show()
-    sys.exit(app.exec())
-
-
-
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
